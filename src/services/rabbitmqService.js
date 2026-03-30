e4257726f918b8d8ed1b0229623182d93e72bc60
ikorex3 <mega_IFOREX@ukr.net>
2026-03-31 00:11:50 +0200

    lab4 part 1

--- src/services/rabbitmqService.js
+++ src/services/rabbitmqService.js
@@ -1,14 +1,17 @@
 const amqp = require('amqplib');
+const { updateTaskStatus, saveTaskResult } = require('../models/db');
 
 let channel = null;
 const QUEUE_NAME = 'transcription.request';
+const EVENTS_QUEUE = 'transcription.events';
 
 async function connectRabbitMQ() {
     try {
         const connection = await amqp.connect('amqp://localhost');
         channel = await connection.createChannel();
         await channel.assertQueue(QUEUE_NAME, { durable: true });
-        console.log('RabbitMQ connected and queue ensured');
+        await channel.assertQueue(EVENTS_QUEUE, { durable: true });
+        console.log('RabbitMQ connected and queues ensured');
     } catch (error) {
         console.error('Failed to connect to RabbitMQ', error);
     }
@@ -31,7 +34,42 @@ async function publishTask(taskData) {
     }
 }
 
+async function consumeEvents() {
+    if (!channel) {
+        console.error('Cannot consume events, RabbitMQ not initialized');
+        return;
+    }
+
+    console.log(`API is listening events in ${EVENTS_QUEUE}`);
+
+    channel.consume(EVENTS_QUEUE, (msg) => {
+        if (msg !== null) {
+            try {
+                const eventData = JSON.parse(msg.content.toString());
+                const { taskId, userId, status, progress, result } = eventData;
+
+                console.log(`[Event] Received update for Task ${taskId}: ${status}`);
+
+                if (status === 'PROCESSING') {
+                    const progressData = JSON.stringify({ progress: progress || 'Started background processing' });
+                    saveTaskResult(taskId, userId, progressData, 'PROCESSING');
+                } else if (status === 'DONE') {
+                    const finalResult = JSON.stringify(result || { message: "Task completed" });
+                    saveTaskResult(taskId, userId, finalResult, 'DONE');
+                } else if (status === 'ERROR') {
+                    updateTaskStatus(taskId, userId, 'ERROR');
+                }
+
+                channel.ack(msg);
+            } catch (error) {
+                console.error(`[Event] Failed to process message`, error);
+                channel.ack(msg);
+            }
+        }, { noAck: false });
+}
+
 module.exports = {
     connectRabbitMQ,
-    publishTask
+    publishTask,
+    consumeEvents
 };
